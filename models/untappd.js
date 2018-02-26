60240dd47af2ec77b783e7d557a60c0d717659e8
Travis Roberts <travis.roberts2@gmail.com>
2018-02-26 01:18:40 -0600

    made api routes to add and remove fav beers from db, added favs button and two possible post requests, add or delete, on click

---
+++ models/untappd.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var BeerInfo = sequelize.define("BeerInfo", {
+
+    beer_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2],
+          msg: "Beer could not be saved as this entry is missing required data! Please choose another."
+        }
+      }
+    },
+    beer_desc: {
+      type: DataTypes.STRING(510)
+    },
+    beer_abv: {
+      type: DataTypes.INTEGER
+    },
+    beer_labelUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [2],
+          msg: "Beer could not be saved as this entry is missing required data! Please choose another."
+        }
+      }
+    },
+    brewery_name: {
+      type: DataTypes.STRING
+    }
+  }, {
+    freezeTableName: true
+  });
+
+return BeerInfo;
+};
