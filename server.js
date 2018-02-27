52dc0f4f70ad9ca0cd458b869a865fd18db09b9b
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-02-26 18:24:00 -0600

    draft

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.set("view engine", "handlebars");
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
-<<<<<<< HEAD
 
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
@@ -28,8 +27,6 @@ db.sequelize.sync({ force: false }).then(function() {
   });
  });
 
-=======
->>>>>>> 97c7b7df296d4cfc569e135a22c646fff257e4c5
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
