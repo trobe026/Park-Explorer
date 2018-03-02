83925537936bfb339b67522f98f2d7c9acac1c65
David Estlund <destlund@gmail.com>
2018-03-01 19:46:09 -0600

    Got the directions list to display brewery names

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ var options;
 var map;
 var markers = [];
 var mapCenter;
+var travelModeSelection = "BICYCLING";
 
 $('#map').css({
     'display': 'inline-block',
@@ -17,35 +18,8 @@ $('#map').css({
 //Search on-click reveals the map and column with information.
 $('#submitButton').on('click', function(){
     event.preventDefault();
-    $.post("/api/brewery-search", $('#brewery-search').text, function(){
-        markers = [];
-        var infoWindow = new google.maps.InfoWindow();
-        //Display Map
-
-        $('#map').css({
-            'display': 'inline-block',
-            'width': '100%',
-            'height': '400px',
-            'border': 'solid',
-            // 'border-left': 'none',
-            'margin-top': '20px'
-        });
-
-        $('#images').css({
-            'display': 'inline-block',
-            'width': '100%',
-            'height': '347px',
-            'margin-top': '20px',
-            'overflow': 'scroll'
-        })
-        $('html, body').animate({
-            scrollTop: $('#searchForm').offset().top
-        }, 1000);
-        $('#frontPage').css({
-            'height': '100%'
-        });
-        $('#infoWindowHeader').css({
-            'color': '#fcbd20;'
-        });
+    $.post("/api/brewery-search", $('#brewery-search').text, function(req, res){
+        console.log("this is where the response goes");
+        console.log(res)
     });
 });
