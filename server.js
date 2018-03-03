8cbbbc482f8fa69376e13b3e74a61e3f8a2385ea
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-03-03 00:10:25 -0600

    draft

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 var favicon = require('serve-favicon')
 var db = require("./models");
+var mysql = require("mysql")
 
 app.use(express.static("public"));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-
+app.use(favicon(path.join(__dirname, 'public')))
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
